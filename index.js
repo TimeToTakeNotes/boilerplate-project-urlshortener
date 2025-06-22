55f851f45f5c39aeea810a74240f2239208d1035
Arno Christie <arno.christie@gmail.com>
2025-06-22 22:50:19 +0200

    Finished URL Shortener Microservice Project
    Implemented all required API endpoints for the URL Shortener Microservice project:
    - POST /api/shorturl→ shortens url and stores both original and shortened urls in url database/array.
    - GET /api/shorturl/:short_url → checks url database/array for existing match and redirects to original url's website.

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { URL } = require('url');
+const dns = require('dns');
+
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(express.json());
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+// Body parser middleware
+app.use(express.urlencoded({ extended: false }));
+
+// In-memory URL store
+let url_db = [];
+let url_id = 1;
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -17,6 +27,43 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+// Shorten a valid url
+app.post('/api/shorturl', (req, res) => {
+  const og_url = req.body.url;
+
+  try {
+    const parsed_url = new URL(og_url);
+    const hostname = parsed_url.hostname;
+
+    dns.lookup(hostname, (err) => {
+      if (err) {
+        return res.json({error: 'invalid url'});
+      }
+      
+      const short_url = url_id++;
+      url_db.push({og_url, short_url});
+
+      res.json({
+        orignal_url: og_url,
+        short_url: short_url
+      });
+    }); 
+  } catch(err) {
+    res.json({error: 'invalid url'});
+  };
+});
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const short = parseInt(req.params.short_url);
+  const entry_point = url_db.find((item) => item.short_url === short);
+
+  if (entry_point) {
+    res.redirect(entry_point.og_url);
+  } else {
+    res.status(404).json({ error: 'No short URL found for given input' });
+  }
 });
 
 app.listen(port, function() {
